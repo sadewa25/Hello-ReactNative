3e3454af58476031adae4f0714b029801b73e076
Muhammad Sadewa Wicaksana W <wicaksanasadewa@yahoo.com>
2020-09-04 15:17:53 +0700

    styling

--- App.js
+++ App.js
@@ -1,8 +1,52 @@
 import React, {Component} from 'react';
-import {Text, View, Image, TextInput} from 'react-native';
+import {Text, View, Image, TextInput, StyleSheet} from 'react-native';
 
 //Component, dan hanya boleh return 1 Induk. 
 const App = () => {
+  return (
+    <StylingReactNativeComponent/>
+  );
+};
+
+const StylingReactNativeComponent = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Styling Object</Text>
+      <View style={{
+        width: 212,
+        backgroundColor: '#f4f4f4', 
+        paddingVertical: 6, 
+        borderRadius: 5, 
+        marginTop: 20,
+        marginLeft: 20,
+        marginRight: 20}}>
+        
+        <Image source={{uri: 'https://cf.bstatic.com/images/hotel/max1024x768/183/183731414.jpg'}}
+        style={{width: 188, height: 100}} />
+        <Text>
+          New Macbook Pro 2019
+        </Text>
+
+        <Text>
+          Mahal Gk Usah Beli
+        </Text>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#10ac84',
+    marginLeft: 20,
+    marginTop: 40,
+  }
+});
+
+const SampleComponent = () => {
   return (
     <View>
       <View style={{width:80, height:80, backgroundColor: 'blue'}}/>
@@ -20,7 +64,7 @@ const App = () => {
       <Profile/>
     </View>
   );
-};
+}
 
 //Functional Component
 const Home = () => {
